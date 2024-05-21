dfb1ba4abe5a8b8d2fae2e81eb57f54338f80489
Jonathan P. Navarrete <jonatns@gmail.com>
2024-05-20 23:48:17 -0400

    Add PlasmoStorageAdapter

---
+++ src/adapters/PlasmoStorageAdapter.ts
@@ -0,0 +1,51 @@
+import { Intent, StorageAdapter } from "../types";
+import { Storage } from "@plasmohq/storage";
+
+export class PlasmoStorageAdapter implements StorageAdapter {
+  private storage: Storage;
+  private key: string;
+
+  constructor(key: string) {
+    this.key = key;
+    this.storage = new Storage({
+      area: "local",
+    });
+  }
+
+  async save(intent: Intent): Promise<void> {
+    const intents = await this.getAllIntents();
+
+    if (intent.id) {
+      const newIntents = intents.map((existingIntent) => {
+        if (existingIntent.id === intent.id) {
+          return {
+            ...existingIntent,
+            ...intent,
+          };
+        }
+        return existingIntent;
+      });
+
+      return this.storage.set(this.key, newIntents);
+    } else {
+      intents.push(
+        structuredClone({
+          ...intent,
+          id: Math.random().toString(36).substring(7),
+          timestamp: Date.now(),
+        })
+      );
+
+      return this.storage.set(this.key, intents);
+    }
+  }
+
+  async getAllIntents(): Promise<Intent[]> {
+    return this.storage.get(this.key);
+  }
+
+  async getIntentsByAddresses(addresses: string[]): Promise<Intent[]> {
+    const intents = await this.getAllIntents();
+    return intents.filter((intent) => addresses.includes(intent.address));
+  }
+}
